1de710d1503a53ba68bd0a5705553bc8f5e27d4b
sihan Ma <you@example.com>
2024-01-06 23:37:35 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box">
+        <div id="text">some quote</div>
+        <div id="author">some author</div>
+        <div id="button">
+          <div><button id="new-quote">New quote</button></div>
+          <div><a id="tweet-quote" href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22It%20is%20never%20too%20late%20to%20be%20what%20you%20might%20have%20been.%22%20George%20Eliot"><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a></div>
+        </div>
+      </div>
     </div>
   );
 }
